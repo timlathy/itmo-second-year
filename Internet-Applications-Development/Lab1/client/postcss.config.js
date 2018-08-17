1ef685c4fc552ea7c33e6b6d216f3e7af7b07a95
timlathy <tim.labushev@gmail.com>
2018-08-17 11:46:40 +0700

    [iad lab1] set up postcss

---
+++ Internet-Applications-Development/Lab1/client/postcss.config.js
@@ -0,0 +1,23 @@
+const production = process.env.NODE_ENV === 'production';
+
+module.exports = {
+  map: !production,
+  plugins: {
+    'postcss-import': {
+      onImport(sources) { global.watchCSS(sources, this.from); }
+    },
+
+    'postcss-preset-env': {
+      features: {
+        'custom-properties': { preserve: false },
+        'nesting-rules': true
+      }
+    },
+
+    'cssnano': production && {
+      preset: ['default', {
+        discardComments: { removeAll: true }
+      }]
+    }
+  }
+}
