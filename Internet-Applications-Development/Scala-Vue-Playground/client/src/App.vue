a0d48cf7205d0699729a4681403bdcf7acde9b07
timlathy <tim.labushev@gmail.com>
2018-09-24 07:45:21 +0300

    [scala-vue-playground] set up vue/rollup/postcss

---
+++ Internet-Applications-Development/Scala-Vue-Playground/client/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+<header><h2>Hello</h2></header>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style>
+:root {
+  --body-font: 'Merriweather', serif;
+}
+
+body {
+  font-family: var(--body-font);
+}
+</style>
