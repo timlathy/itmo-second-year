56833d523f88f6c39a3d50a20e0e9f82dc5c494a
timlathy <tim.labushev@gmail.com>
2018-11-06 06:56:32 +0300

    [spl lab5] use a macro to shorten error-handling code

--- System-Programming-Languages/Lab5/main.c
+++ System-Programming-Languages/Lab5/main.c
@@ -3,20 +3,16 @@
 #include "image.h"
 #include "image_bmp.h"
 
+#define PANIC(...) do { fprintf(stderr, __VA_ARGS__); return 1; } while (0)
+
 int main(int argc, char** argv) {
-  if (argc != 3) {
-    fprintf(stderr, "Usage: lab5 source.bmp destination.bmp\n");
-    return 1;
-  }
+  if (argc != 3) PANIC("Usage: lab5 source.bmp destination.bmp\n");
 
   const char* input_path = argv[1];
   const char* output_path = argv[2];
 
   FILE* input_file = fopen(input_path, "rb");
-  if (input_file == NULL) {
-    fprintf(stderr, "Unable to open %s for reading\n", input_path);
-    return 1;
-  }
+  if (input_file == NULL) PANIC("Unable to open %s for reading\n", input_path);
 
   image_t input_image;
 
@@ -25,36 +21,27 @@ int main(int argc, char** argv) {
 
   switch (read_status) {
     case READ_INVALID_SIGNATURE:
-      fprintf(stderr, "%s is not a valid BMP file\n", input_path);
-      return 1;
+      PANIC("%s is not a valid BMP file\n", input_path);
     case READ_UNSUPPORTED_BITS_PER_PIXEL:
-      fprintf(stderr, "Only 24-bit (8 bits per channel) source images are supported\n");
-      return 1;
+      PANIC("Only 24-bit (8 bits per channel) source images are supported\n");
     case READ_ERROR:
-      fprintf(stderr, "Encountered an error while opening %s\n", input_path);
-      return 1;
-    default:
+      PANIC("Encountered an error while opening %s\n", input_path);
+    case READ_OK:
       break;
   }
 
   image_t image_rot = image_rotate_copy_90cw(input_image);
 
   FILE* output_file = fopen(output_path, "wb");
-  if (output_file == NULL) {
-    fprintf(stderr, "Unable to open %s for writing\n", output_path);
-    return 1;
-  }
+  if (output_file == NULL) PANIC("Unable to open %s for writing\n", output_path);
 
   bmp_write_status_t write_status = image_write_bmp(output_file, image_rot);
   fclose(output_file);
 
   switch (write_status) {
     case WRITE_ERROR:
-      fprintf(stderr, "Encountered an error while saving the destination image\n");
-      return 1;
-    default:
-      break;
+      PANIC("Encountered an error while saving the destination image\n");
+    case WRITE_OK:
+      return 0;
   }
-
-  return 0;
 }
