ced8f076ac396818f9314fe297f168f55283a163
timlathy <tim.labushev@gmail.com>
2018-12-03 20:35:53 +0300

    [spl lab7] benchmark mode

--- System-Programming-Languages/Lab7/main.c
+++ System-Programming-Languages/Lab7/main.c
@@ -5,13 +5,52 @@
 #include "image_bmp.h"
 #include "image_sepia.h"
 
+#include <sys/time.h>
+#include <sys/resource.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
+
 #define PANIC(...) do { fprintf(stderr, __VA_ARGS__); return 1; } while (0)
 
-int main(int argc, char** argv) {
-  if (argc != 3) PANIC("Usage: lab7 source.bmp destination.bmp\n");
+#define BENCHMARK_NUM_RUNS 250UL
 
-  const char* input_path = argv[1];
-  const char* output_path = argv[2];
+void do_benchmark(image_t* img) {
+  struct rusage r;
+  struct timeval start, end;
+
+  getrusage(RUSAGE_SELF, &r); start = r.ru_utime;
+
+  for (uint64_t i = 0; i < BENCHMARK_NUM_RUNS; i++) sepia_sse_inplace(img);
+
+  getrusage(RUSAGE_SELF, &r); end = r.ru_utime;
+
+  long res = ((end.tv_sec - start.tv_sec) * 1000000L) + end.tv_usec - start.tv_usec;
+
+  printf("SSE implementation: %ld μs (average for %lu runs)\n", res / BENCHMARK_NUM_RUNS, BENCHMARK_NUM_RUNS);
+
+  // ---
+
+  getrusage(RUSAGE_SELF, &r); start = r.ru_utime;
+
+  for (uint64_t i = 0; i < BENCHMARK_NUM_RUNS; i++) sepia_naive_inplace(img);
+
+  getrusage(RUSAGE_SELF, &r); end = r.ru_utime;
+
+  res = ((end.tv_sec - start.tv_sec) * 1000000L) + end.tv_usec - start.tv_usec;
+
+  printf("Naive implementation: %ld μs (average for %lu runs)\n", res / BENCHMARK_NUM_RUNS, BENCHMARK_NUM_RUNS);
+}
+
+const char* USAGE = "Usage: lab7 (naive|sse|benchmark) src.bmp dst.bmp\n";
+
+int main(int argc, char** argv) {
+  if (argc != 4 || (strncmp(argv[1], "naive", 5) != 0
+        && strncmp(argv[1], "sse", 3) != 0
+        && strncmp(argv[1], "benchmark", 9) != 0)) PANIC(USAGE);
+
+  const char* input_path = argv[2];
+  const char* output_path = argv[3];
 
   FILE* input_file = fopen(input_path, "rb");
   if (input_file == NULL) PANIC("Unable to open %s for reading\n", input_path);
@@ -32,10 +71,15 @@ int main(int argc, char** argv) {
       break;
   }
 
-  sepia_sse_inplace(&img);
-
   FILE* output_file = fopen(output_path, "wb");
   if (output_file == NULL) PANIC("Unable to open %s for writing\n", output_path);
+
+  if (strncmp(argv[1], "naive", 5) == 0) sepia_naive_inplace(&img);
+  else if (strncmp(argv[1], "sse", 3) == 0) sepia_sse_inplace(&img);
+  else {
+    do_benchmark(&img);
+    return 0;
+  }
 
   bmp_write_status_t write_status = image_write_bmp(output_file, img);
   fclose(output_file);
