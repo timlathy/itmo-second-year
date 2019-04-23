a7370f1ba7743fb3c6fb3f7c6c615993c4b8c0da
timlathy <tim.labushev@gmail.com>
2019-04-23 12:21:16 +0300

    [ic lab2] 16-bit square root module

---
+++ Introduction-to-Digital-IC-Design/Lab2/lab2.srcs/sim_1/new/sqrt-testbench.v
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+
+module sqrt_testbench;
+    reg clk_in, rst_in, start_in;
+    reg [15:0] x_in;
+    wire busy_out;
+    wire [15:0] sqrt_out;
+    
+    sqrt s(
+        .clk(clk_in), .rst(rst_in), .start(start_in), .x_in(x_in),
+        .busy(busy_out), .out(sqrt_out)
+    );
+    
+    initial begin
+        clk_in = 0;
+        forever #8 clk_in = !clk_in; // alternate clock every 8ns
+    end
+    
+    integer x_sqrt;
+    
+    initial begin
+        for (x_sqrt = 1; x_sqrt < 255; x_sqrt = x_sqrt + 1) begin
+            for (x_in = x_sqrt * x_sqrt; x_in < (x_sqrt + 1) * (x_sqrt + 1); x_in = x_in + 1) begin
+                repeat(1) @(posedge clk_in);
+                rst_in = 1'b1;
+                
+                repeat(1) @(posedge clk_in);
+                rst_in = 1'b0;
+                start_in = 1'b1;
+                
+                repeat(1) @(posedge clk_in);
+                
+                while (busy_out)
+                    repeat (1) @(posedge clk_in);
+                    
+                if (sqrt_out == x_sqrt)
+                    $display("[+] sqrt(%d) = %d", x_in, sqrt_out);
+                else begin
+                    $display("Expected %d for sqrt(%d), got %d", x_sqrt, x_in, sqrt_out);
+                    $stop;
+                end
+            end
+        end
+        $finish;
+    end
+endmodule
