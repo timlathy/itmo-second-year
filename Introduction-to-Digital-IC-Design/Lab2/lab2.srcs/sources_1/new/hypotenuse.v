224a20b9ef95e12fefe112b7475ffe19759be49a
timlathy <tim.labushev@gmail.com>
2019-04-25 15:14:57 +0300

    [ic lab2] simplify the wiring a bit

--- Introduction-to-Digital-IC-Design/Lab2/lab2.srcs/sources_1/new/hypotenuse.v
+++ Introduction-to-Digital-IC-Design/Lab2/lab2.srcs/sources_1/new/hypotenuse.v
@@ -14,26 +14,26 @@ module hypotenuse(
     reg [3:0] state;
     
     localparam Idle = 3'b000;
-    localparam BeginMul = 3'b001;
-    localparam WaitMul = 3'b010;
+    localparam BeginSquare = 3'b001;
+    localparam WaitSquare = 3'b010;
     localparam BeginSqrt = 3'b011;
     localparam WaitSqrt = 3'b100;
     
     assign busy = state != Idle;
     
-    reg sq1_rst, sq1_start, sq2_rst, sq2_start;
+    reg sq12_rst, sq12_start;
     reg [7:0] sq1_in, sq2_in;
     
     wire sq1_busy, sq2_busy;
     wire [15:0] sq1_out, sq2_out;
     
     mul sq1(
-        .clk(clk), .rst(sq1_rst), .start(sq1_start),
+        .clk(clk), .rst(sq12_rst), .start(sq12_start),
         .a_in(sq1_in), .b_in(sq1_in), .busy(sq1_busy), .out(sq1_out)
     );
     
     mul sq2(
-        .clk(clk), .rst(sq2_rst), .start(sq2_start),
+        .clk(clk), .rst(sq12_rst), .start(sq12_start),
         .a_in(sq2_in), .b_in(sq2_in), .busy(sq2_busy), .out(sq2_out)
     );
     
@@ -50,29 +50,26 @@ module hypotenuse(
     always@(posedge clk)
         if (rst) begin
             state <= Idle;
-            sq1_rst <= 1;
-            sq2_rst <= 1;
+            sq12_rst <= 1;
             sqrt_rst <= 1;
         end
         else case (state)
             Idle:
                 if (start) begin
-                    state <= BeginMul;
+                    state <= BeginSquare;
                     sq1_in <= a_in;
                     sq2_in <= b_in;
-                    sq1_rst <= 0;
-                    sq2_rst <= 0;
-                    sq1_start <= 1;
-                    sq2_start <= 1;
+                    sq12_rst <= 0;
+                    sq12_start <= 1;
                 end
-            BeginMul:
-                state <= WaitMul;
-            WaitMul:
+            BeginSquare:
+                state <= WaitSquare;
+            WaitSquare:
                 if (!sq1_busy && !sq2_busy) begin
                     state <= BeginSqrt;
                     sq_sum <= sq1_out + sq2_out;
                     sqrt_rst <= 0;
-                    sqrt_start = 1;
+                    sqrt_start <= 1;
                 end
             BeginSqrt:
                 state <= WaitSqrt;
