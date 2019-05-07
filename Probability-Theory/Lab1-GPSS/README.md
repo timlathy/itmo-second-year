e31b7ed35248ff438874f9dc1161033b351622b8
timlathy <tim.labushev@gmail.com>
2019-05-07 16:17:17 +0300

    [pt lab1] the first table includes T_RAN only

--- Probability-Theory/Lab1-GPSS/README.md
+++ Probability-Theory/Lab1-GPSS/README.md
@@ -16,17 +16,21 @@ I decided to take a stab at automating this with [Pywinauto](https://pywinauto.g
 pip install --user Pillow pywinauto
 ```
 
-## Generating `result.json`
+## Generating `.json` results
 
 1. Place `main.py` alongside `rexp.gps` and `erlang.gps`
-2. Open `main.py` in an editor and substitute the value of `GPSS_PATH` with the path to your GPSS World installation
+2. Create copies of `rexp.gps` for your variants (e.g. `rexp100.gps` and `rexp200.gps`).
+Replace `RN1` with the parameter you're given for `T_RAN`, and `Exponential(1,` with the same parameter for `T_EXP`.
+3. Create copies of `erlang.gps` for each of your variants and `k` parameters (`erlang100_k2.gps`, `erlang100_k4.gps`, ...)
+2. Open `main.py` in an editor and substitute the value of `GPSS_PATH` with the path to your GPSS World installation,
+and the value of `LAB_FILES` with the copies you've just created.
 3. Run `python3 main.py`
 
 ## Creating the report
 
 This can be done on Linux (`pywin32` and `pywinauto` packages are not needed).
 
-1. Place `table.py` alongside your `result.json`
+1. Place `table.py` alongside your result files
 2. Run `python3 table.py`
 3. Create a TeX file to wrap the generated `table1.tex` (refer to `report.tex` as an example)
 4. Render it (`xelatex report.tex`)
--- Probability-Theory/Lab1-GPSS/table.py
+++ Probability-Theory/Lab1-GPSS/table.py
@@ -3,6 +3,8 @@ import json
 import math
 from tabulate import tabulate
 
+TABLE1 = [('rexp176.gps.json', 176), ('rexp924.gps.json', 924)]
+
 def num(n):
     return str(round(n, 4))
 
@@ -12,28 +14,32 @@ def numcells(cells):
 rowend = '\\\\\\hline\n'
 partrowend = '\\\\\\cline{2-13}\n'
 
-def gen_table1(rn1, rn2):
-    with open('result.json') as f:
-        data = json.load(f)
+def gen_table1():
+    file1, rn1 = TABLE1[0]
+    file2, rn2 = TABLE1[1]
+
+    with open(file1) as f:
+        data1 = json.load(f)
+    with open(file2) as f:
+        data2 = json.load(f)
+
+    reports = [r['T_RAN'] for r in data1['reports']] + [r['T_RAN'] for r in data2['reports']]
 
     mean_exp = 500
     stddev_exp = 1000 / (math.sqrt(3) * 2)
     variability_exp = stddev_exp / mean_exp
 
-    mean = [r['ran']['mean'] for r in data['reports']] +\
-        [r['exp']['mean'] for r in data['reports']]
+    mean = [r['mean'] for r in reports]
     mean_diff = [(m - mean_exp) / mean_exp for m in mean]
 
-    stddev = [r['ran']['stddev'] for r in data['reports']] +\
-        [r['exp']['stddev'] for r in data['reports']]
+    stddev = [r['stddev'] for r in reports]
     stddev_diff = [(s - stddev_exp) / s for s in stddev]
 
-    variability = [r['ran']['stddev'] / r['ran']['mean'] for r in data['reports']] +\
-        [r['exp']['stddev'] / r['exp']['mean'] for r in data['reports']]
+    variability = [r['stddev'] / r['mean'] for r in reports]
     variability_diff = [(v - variability_exp) / variability_exp for v in variability]
 
     t = '\\begin{tabular}{|l*{12}{|c}|}\\hline\n'
-    t += '\\multirow{2}{*}{Хар-ки и интервалы} & \\multicolumn{6}{c|}{RN ' + rn1 + '} & \\multicolumn{6}{c|}{RN ' + rn2 + '}\\\\\\cline{2-13}\n'
+    t += '\\multirow{2}{*}{Хар-ки и интервалы} & \\multicolumn{6}{c|}{RN ' + num(rn1) + '} & \\multicolumn{6}{c|}{RN ' + num(rn2) + '}\\\\\\cline{2-13}\n'
     t += '& 10 & 100 & 1000 & 5000 & 10000 & 20000 & 10 & 100 & 1000 & 5000 & 10000 & 20000' + rowend
     # mean
     t += '\\multirow{2}{*}{Мат. ож = ' + num(mean_exp) + '} & ' + numcells(mean) + partrowend
@@ -45,12 +51,11 @@ def gen_table1(rn1, rn2):
     t += '\\multirow{2}{*}{К-т вар. = ' + num(variability_exp) + '} & ' + numcells(variability) + partrowend
     t += '& ' + numcells(variability_diff) + rowend
     for i, rstart in enumerate(range(0, 1000, 100)):
-        freqs = [r['ran']['freqs'][i] for r in data['reports']] +\
-            [r['exp']['freqs'][i] for r in data['reports']]
+        freqs = [r['freqs'][i] for r in reports]
         t += num(rstart) + '-' + num(rstart + 100) + ' & ' + numcells(freqs) + rowend
     t += '\\end{tabular}'
 
     with open('table1.tex', 'w') as f:
         f.write(t)
 
-gen_table1('10', '20')
+gen_table1()
