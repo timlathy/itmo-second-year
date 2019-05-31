8e9a4e702f68298a1a6056f27208e07b1df9d5e6
timlathy <tim.labushev@gmail.com>
2019-05-31 20:55:51 +0300

    [algos] 1080: looks good doesn't work (tle)

--- Algorithms-and-Data-Structures/Graphs/1080.cpp
+++ Algorithms-and-Data-Structures/Graphs/1080.cpp
@@ -4,20 +4,25 @@
 #define uint unsigned int
 
 struct AdjacencyMatrix {
-  uint edges;
+  uint vertices;
   std::vector<bool> matrix;
 
-  AdjacencyMatrix(uint n) : edges(n), matrix(n * n) {}
+  AdjacencyMatrix(uint v) : vertices(v), matrix(v * v) {}
 
   void connect(uint a, uint b) {
-    this->matrix[a * edges + b] = true;
+    this->matrix[a * vertices + b] = true;
+    this->matrix[b * vertices + a] = true;
+  }
+  
+  bool exists(uint a, uint b) const {
+    return this->matrix[a * vertices + b];
   }
 
-  void print() {
-    for (uint i = 0; i < edges; i++) {
+  void print() const {
+    for (uint i = 0; i < vertices; ++i) {
       std::cout << i << ":";
-      for (uint j = 0; j < edges; j++) {
-        if (matrix[i * edges + j])
+      for (uint j = 0; j < vertices; ++j) {
+        if (matrix[i * vertices+ j])
           std::cout << " " << j;
       }
       std::cout << std::endl;
@@ -25,23 +30,85 @@ struct AdjacencyMatrix {
   }
 };
 
+struct BinaryVertexColors {
+  uint colored;
+  std::vector<bool> colors;
+
+  BinaryVertexColors(uint v) : colored(0), colors(v) {}
+
+  bool has_neighbors_of_color(uint v, bool color, AdjacencyMatrix const &edges) const {
+    for (uint i = 0; i < colored; ++i)
+      if (edges.exists(v, i) && color == colors[i])
+        return true;
+    return false;
+  }
+
+  void set_color(uint v, bool color) {
+    colors[v] = color;
+    colored = v + 1;
+  }
+
+  void remove_color(uint v) {
+    colored = v - 1;
+  }
+   
+  std::string get_color_string() const {
+    if (colored == 0 || colored < colors.size())
+      return "-1";
+
+    std::string color_string(colors.size(), '0');
+    for (uint i = 0; i < colors.size(); ++i)
+      if (colors[i])
+        color_string[i] = '1';
+    return color_string;
+  }
+};
+
 int main() {
-  uint n;
-  std::cin >> n;
+  uint v;
+  std::cin >> v;
 
-  AdjacencyMatrix edges(n);
+  AdjacencyMatrix edges(v);
+  BinaryVertexColors vertices(v);
 
-  for (uint i = 0; i < n; ++i) {
-    uint j;
+  for (uint i = 0; i < v; ++i) {
+    uint edge;
 
     while (true) {
-      std::cin >> j;
-      if (j == 0)
+      std::cin >> edge;
+      if (edge == 0)
         break;
       else
-        edges.connect(i, j - 1);
+        edges.connect(i, edge - 1);
     }
   }
 
-  edges.print();
+  while (vertices.colored != v) {
+    if (!vertices.has_neighbors_of_color(vertices.colored, false, edges)) {
+      vertices.set_color(vertices.colored, false);
+    }
+    else if (!vertices.has_neighbors_of_color(vertices.colored, true, edges)) {
+      vertices.set_color(vertices.colored, true);
+    }
+    else if (vertices.colored > 0 && vertices.colors[vertices.colored - 1] == false &&
+        !vertices.has_neighbors_of_color(vertices.colored - 1, true, edges)) {
+      vertices.set_color(vertices.colored - 1, true);
+    }
+    else {
+      bool backtracked = false;
+      while (vertices.colored > 1) {
+        vertices.remove_color(vertices.colored);
+        if (vertices.colors[vertices.colored] == false &&
+            !vertices.has_neighbors_of_color(vertices.colored, true, edges)) {
+          vertices.set_color(vertices.colored, true);
+          backtracked = true;
+          break;
+        }
+      }
+      if (!backtracked) break;
+    }
+  }
+
+  std::cout << vertices.get_color_string() << std::endl;
+  return 0;
 }
