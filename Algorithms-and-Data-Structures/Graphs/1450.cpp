e9f2899dc2881ac3a3ce5b012393f43586ffbd25
timlathy <tim.labushev@gmail.com>
2019-06-01 01:51:58 +0300

    [algos] 1450 wip: bellman-ford

--- Algorithms-and-Data-Structures/Graphs/1450.cpp
+++ Algorithms-and-Data-Structures/Graphs/1450.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <vector>
-#include <set>
 
 #define uint unsigned int
 #define ushort unsigned short
@@ -12,70 +11,35 @@ struct Edge {
 };
 
 int main() {
-  uint vs, es, v_from, v_to;
-  std::cin >> vs >> es;
+  uint v_count, e_count;
+  std::cin >> v_count >> e_count;
 
-  std::vector<Edge> edges(es);
-  for (uint i = 0; i < es; ++i) {
+  std::vector<Edge> edges(e_count);
+  for (uint i = 0; i < e_count; ++i) {
     ushort a, b, weight;
     std::cin >> a >> b >> weight;
     edges[i] = {a - 1, b - 1, weight};
   }
 
-  std::cin >> v_from >> v_to;
-  --v_from; --v_to;
+  ushort v_start, v_dest;
+  std::cin >> v_start >> v_dest;
+  --v_start;
+  --v_dest;
 
-  std::vector<bool> visited(vs);
-  std::vector<int> weights(vs, -1);
+  std::vector<int> weights(v_count, -1);
+  weights[v_start] = 0;
 
-  uint v = v_from;
-  uint total_visited = 0;
-
-  weights[v] = 0;
-
-  while (total_visited != vs) {
-    if (!visited[v]) {
-      visited[v] = true;
-      total_visited++;
+  for (uint v = 1; v < v_count; ++v) {
+    for (auto edge : edges) {
+      if (weights[edge.a] != -1 && weights[edge.a] + edge.weight > weights[edge.b])
+        weights[edge.b] = weights[edge.a] + edge.weight;
     }
-    for (uint i = 0; i < es; ++i) {
-      if (edges[i].a == v) {
-        ushort v2 = edges[i].b;
-        if (weights[v] + edges[i].weight > weights[v2]) {
-          weights[v2] = weights[v] + edges[i].weight;
-          if (visited[v2]) {
-            visited[v2] = false;
-            total_visited--;
-          }
-        }
-      }
-    }
-    int next_v = -1, max_weight = 0;
-    for (uint vis = 0; vis < vs; ++vis) {
-      if (!visited[vis]) continue;
-
-      for (uint e = 0; e < es; ++e) {
-        if (edges[e].a == vis && !visited[edges[e].b] && edges[e].weight > max_weight) {
-          max_weight = edges[e].weight;
-          next_v = edges[e].b;
-        }
-      }
-    }
-    if (next_v == -1) {
-      break;
-    }
-    v = next_v;
   }
 
-
-  if (weights[v_to] == -1)
+  if (weights[v_dest] == -1)
     std::cout << "No solution" << std::endl;
   else
-    std::cout << weights[v_to] << std::endl;
-
-  //for (int i = 0; i < weights.size(); ++i) {
-  //  std::cout << i << ": " << weights[i] << std::endl;
-  //}
+    std::cout << weights[v_dest] << std::endl;
 
   return 0;
 }
