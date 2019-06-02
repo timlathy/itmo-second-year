113ba8374a0e4b4efe50428087b7d963ea07d743
timlathy <tim.labushev@gmail.com>
2019-06-02 12:34:48 +0300

    [sys cw] native words: stack operations

--- System-Software/Coursework-Forth-Interpreter/interp.asm
+++ System-Software/Coursework-Forth-Interpreter/interp.asm
@@ -8,25 +8,7 @@ section .bss
 resq 1023
 rstack_start: resq 1
 
-%include "dict.inc"
-
-native 'exit', exit, 0
-  mov pc, [rstack]
-  add rstack, 8
-  jmp next
-
-native '+', plus, 0
-  pop rax
-  add [rsp], rax
-  jmp next
-
-native '.', dot, 0
-  pop rdi
-  call print_int
-  jmp next
-
-native 'bye', bye, 0
-  call system_exit
+%include "native-words.inc"
 
 section .text
 global _start
@@ -44,11 +26,12 @@ docol:
   jmp next
 
 program:
-  dq xt_plus, xt_dot, xt_bye
+  dq xt_drop, xt_plus, xt_dot, xt_bye
 
 _start:
   mov rstack, rstack_start
   mov pc, program
   push 5
   push 7
+  push 9
   jmp next
